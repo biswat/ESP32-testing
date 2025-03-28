aa12eee701573a58f1307cd2e88acd28f21e4731
Tanbir Pradhan <pradhantanbir@gmail.com>
2025-03-28 11:09:26 +0530

    addedMQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,125 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 
+// WiFi credentials
+const char* ssid = "V2029";  
+const char* password = "tanbir123";  
 
-const char* ssid = "ISU_HQ";  // Replace with your WiFi SSID
-const char* password = "iServeu@8895"; // Replace with your WiFi password
+// HiveMQ Cloud MQTT Broker
+const char* mqtt_server = "3c07990e59c44be0afbc193bdbf9af31.s1.eu.hivemq.cloud";  // Example: "yourcluster.s1.eu.hivemq.cloud"
+const int mqtt_port = 8883;  // Secure TLS port
 
-WebServer server(80);
-const int ledPin = 2;  // Built-in LED pin
+// MQTT credentials (HiveMQ Cloud)
+const char* mqtt_user = "pradhantanbir";  
+const char* mqtt_password = "Tanbir123";  
+const char* mqtt_topic = "esp32/led";  
 
-void handleRoot() {
-  String html = "<html><body>"
-                "<h2>ESP32 LED Control</h2>"
-                "<button onclick=\"toggleLED('on')\">Turn ON</button>"
-                "<br><br><br>"
-                "<button onclick=\"toggleLED('off')\">Turn OFF</button>"
-                "<script>"
-                "function toggleLED(state) {"
-                " fetch('/led?state=' + state);"
-                "}"
-                "</script>"
-                "</body></html>";
-  server.send(200, "text/html", html);
+// Define LED pin
+const int ledPin = 2;  // Built-in LED on most ESP32 boards
+
+// Root CA certificate (HiveMQ Cloud / Let's Encrypt ISRG Root X1)
+const char* ca_cert = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+// Secure WiFi client & MQTT client
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+// Function to handle received MQTT messages
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message received on topic: ");
+    Serial.println(topic);
+
+    String message = "";
+    for (int i = 0; i < length; i++) {
+        message += (char)payload[i];
+    }
+    
+    Serial.print("Message: ");
+    Serial.println(message);
+
+    if (message == "ON") {
+        digitalWrite(ledPin, HIGH);  // Turn LED on
+        Serial.println("LED ON");
+    } else if (message == "OFF") {
+        digitalWrite(ledPin, LOW);  // Turn LED off
+        Serial.println("LED OFF");
+    }
 }
 
-void handleLEDControl() {
-  String state = server.arg("state");
-  if (state == "on") {
-    digitalWrite(ledPin, HIGH);
-  } else if (state == "off") {
-    digitalWrite(ledPin, LOW);
-  }
-  server.send(200, "text/plain", "OK");
+// Function to reconnect to MQTT broker
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Connecting to MQTT... ");
+        if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
+            Serial.println("Connected!");
+            client.subscribe(mqtt_topic);
+        } else {
+            Serial.print("Failed (Error Code: ");
+            Serial.print(client.state());
+            Serial.println("). Retrying in 5 seconds...");
+            delay(5000);
+        }
+    }
 }
 
+// Setup function
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi Connected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+    Serial.begin(115200);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
 
-  pinMode(ledPin, OUTPUT);
-  server.on("/", handleRoot);
-  server.on("/led", handleLEDControl);
-  server.begin();
-  Serial.println("Web Server started.");
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nWiFi connected!");
+
+    espClient.setCACert(ca_cert);  // Load CA certificate
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(callback);
+
+    reconnect();
 }
 
+// Loop function
 void loop() {
-  server.handleClient();
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
 }
